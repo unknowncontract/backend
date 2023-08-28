2b9f0c6716a17df77cc8d58ba80ab8733ae08c53
cocoa-dev-1 <hyeonseung1006@gmail.com>
2023-08-28 10:11:04 +0900

    feat: add config

---
+++ src/config.py
@@ -0,0 +1,18 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from functools import lru_cache
+
+
+class Settings(BaseSettings):
+    db_host: str = ""
+    db_port: str = ""
+    db_username: str = ""
+    db_password: str = ""
+    db_name: str = ""
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+@lru_cache()
+def get_settings():
+    return Settings()
