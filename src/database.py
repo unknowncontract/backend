ee93774e494cc0a9a8862615beb9d5c57b9faa75
cocoa-dev-1 <hyeonseung1006@gmail.com>
2023-08-28 12:22:39 +0900

    refactor: change to absolute path

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .config import get_settings
+from src.config import get_settings
 
 config = get_settings()
 
