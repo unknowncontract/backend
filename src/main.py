cc68085c61ae75530863b54372ab9bc4f9d6665f
cocoa-dev-1 <hyeonseung1006@gmail.com>
2023-08-28 12:42:22 +0900

    feat: set default res class to orjson

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 from src.routes import image
 
-app = FastAPI()
+app = FastAPI(default_response_class=ORJSONResponse)
 
 app.include_router(image.router)
 
 
 @app.get("/")
 async def main():
-    return "root"
+    return ORJSONResponse({"status": 200})
