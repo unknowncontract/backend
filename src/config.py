7d3fe09f081f2cc97785fc5fe09c97c0e8206439
cocoa-dev-1 <hyeonseung1006@gmail.com>
2023-08-28 20:50:32 +0900

    feat: add openai key config

--- src/config.py
+++ src/config.py
@@ -9,6 +9,7 @@ class Settings(BaseSettings):
     db_username: str = ""
     db_password: str = ""
     db_name: str = ""
+    openai_secret_key: str = ""
 
     model_config = SettingsConfigDict(env_file=".env")
 
