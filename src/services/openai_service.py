2fc63cc8a997d16d9b02341d152de553232665e4
cocoa-dev-1 <hyeonseung1006@gmail.com>
2023-08-29 14:41:05 +0900

    feat: add question to openai

--- src/services/openai_service.py
+++ src/services/openai_service.py
@@ -2,9 +2,15 @@ import openai
 from src.core.config import config
 
 
-openai.api_key = config.openai_secret_key
-
-
 class OpenaiService:
     async def get_message(self, message: str) -> str:
-        return message
+        response = openai.ChatCompletion.create(
+            api_key=config.openai_secret_key,
+            model="gpt-4",
+            messages=[
+                {"role": "system", "content": "너는 부동산 전문 변호사야. 답은 무조건 목록 형태로 해."},
+                {"role": "user", "content": message},
+            ],
+        )
+
+        return response["choices"][0]["message"]["content"]
