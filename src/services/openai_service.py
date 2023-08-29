447527718ec081d410fe8b9ed5cf5c1b128bc29c
cocoa-dev-1 <hyeonseung1006@gmail.com>
2023-08-30 08:21:35 +0900

    feat: change gpt model to 3.5 turbo

--- src/services/openai_service.py
+++ src/services/openai_service.py
@@ -6,7 +6,7 @@ class OpenaiService:
     async def get_message(self, message: str) -> str:
         response = openai.ChatCompletion.create(
             api_key=config.openai_secret_key,
-            model="gpt-4",
+            model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": "너는 부동산 전문 변호사야. 답은 무조건 목록 형태로 해."},
                 {"role": "user", "content": message},
